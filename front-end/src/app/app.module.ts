6c38094563efdabdbf654180c711defb75535981
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-02-04 10:27:26 +0530

    app header added.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
